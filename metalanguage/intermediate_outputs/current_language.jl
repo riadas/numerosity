3e260a0d62fce5b748fb5b792b27c3a76845fcce
riadas <riadas@mit.edu>
2025-07-24 18:00:10 -0700

    first plots

---
+++ metalanguage/intermediate_outputs/current_language.jl
@@ -0,0 +1,89 @@
+include("../../base/base_semantics.jl")
+
+function one(set::Union{Exact, Blur})::Bool
+    set.value == 1
+end
+
+function two(set::Union{Exact, Blur})::Bool
+    set.value == 2
+end
+
+function three(set::Union{Exact, Blur})::Bool
+    set.value == 3
+end
+
+function four(set::Union{Exact, Blur})::Bool
+    set.value == 4
+end
+
+function five(set::Exact)::Bool
+    set.value == 5
+end
+
+function six(set::Exact)::Bool
+    not(map(x -> Base.invokelatest(x, set), [one, two, three, four, five]))
+end
+
+function seven(set::Exact)::Bool
+    not(map(x -> Base.invokelatest(x, set), [one, two, three, four, five]))
+end
+
+function eight(set::Exact)::Bool
+    not(map(x -> Base.invokelatest(x, set), [one, two, three, four, five]))
+end
+
+function nine(set::Exact)::Bool
+    not(map(x -> Base.invokelatest(x, set), [one, two, three, four, five]))
+end
+
+function ten(set::Exact)::Bool
+    not(map(x -> Base.invokelatest(x, set), [one, two, three, four, five]))
+end
+
+# ANS
+function five(set::Blur)::Bool
+    true
+end
+
+function six(set::Blur)::Bool
+    true
+end
+
+function seven(set::Blur)::Bool
+    true
+end
+
+function eight(set::Blur)::Bool
+    true
+end
+
+function nine(set::Blur)::Bool
+    true
+end
+
+function ten(set::Blur)::Bool
+    true
+end
+
+function list_syntax_next(word::String)::String 
+    if word == "one"
+    return two
+elseif word == "two"
+    return three
+elseif word == "three"
+    return four
+elseif word == "four"
+    return five
+elseif word == "five"
+    return six
+elseif word == "six"
+    return seven
+elseif word == "seven"
+    return eight
+elseif word == "eight"
+    return nine
+elseif word == "nine"
+    return ten
+end
+
+end
